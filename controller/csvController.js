2048330ba77446a9c12d0a5aab43ae7282a620b4
Ankur261 <ankuragrawal911@gmail.com>
2024-01-29 12:58:39 +0530

    final app

--- controller/csvController.js
+++ controller/csvController.js
@@ -1,14 +1,13 @@
 const csvModel = require('../model/csvModel');
 const csv = require('fast-csv');
-const mongoose = require('../config/mongoose') ;
-const path = require('path') ;
+const mongoose = require('../config/mongoose');
+const path = require('path');
 
 module.exports.homePage = async function (req, res) {
 
     const csvFile = await csvModel.find({});
 
     return res.render('csvHome', { csvFiles: csvFile });
-
 
 
 }
@@ -29,17 +28,17 @@ module.exports.deleteFile = async function (req, res) {
 module.exports.openFile = function (req, res) {
     const fileId = req.params.id;
     csvModel.findById(fileId).then(data => {
-        const parsedData = []; 
+        const parsedData = [];
         csv.parseString(data.fileContent, { headers: true })
             .on('data', (row) => {
                 parsedData.push(row);
             }).on('error', (err) => {
                 console.log(err);
-                return ;
+                return;
             })
             .on('end', () => {
                 const fileHeaders = Object.keys(parsedData[0]);
-                const fileName = path.parse(data.fileName).name ;
+                const fileName = path.parse(data.fileName).name;
                 return res.render('csvView', { fileName: fileName, csvheaders: fileHeaders, fileContent: parsedData });
             });
     });
@@ -47,18 +46,26 @@ module.exports.openFile = function (req, res) {
 }
 
 module.exports.csvFileUpload = function (req, res) {
+    
+    const parsedPath = path.parse(req.file.originalname);
 
-    const csvFileContent = req.file.buffer.toString();
-    const csvFileName = req.file.originalname;
+    if (parsedPath.ext.toLowerCase() === '.csv') {
+        const csvFileContent = req.file.buffer.toString();
+        const csvFileName = req.file.originalname;
+        csvModel.create({
+            fileName: csvFileName,
+            fileContent: csvFileContent
+        }).then(() => {
+            return res.redirect('/csv', {alert: true, message: "CSV file uploaded successfully"});
+        }).catch((err) => {
+            console.log(err);
+            return res.send({alert:"internal server error"})
+        })
+    }
 
-    csvModel.create({
-        fileName: csvFileName,
-        fileContent: csvFileContent
-    }).then(() => {
-        return res.redirect('/csv');
-    }).catch((err) => {
-        console.log(err);
-        return res.send({ upload: "unsucessful" })
-    })
+    else
+
+    return res.json({alert: "Uploaded file extension is not .csv"})
+
 
 }
